577462f7740f229aeff0257fba942b4b762dae5a
mastelo <mirandarodriguezronald@gmail.com>
2020-09-23 12:55:40 -0400

    Updates

---
+++ precache-manifest.fead15cf96c13e6b60e393c4f76ecd83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f026b60a3e65c1188ebf8e515651b01",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3dd337cd11f4ca0d6295",
+    "url": "/my-app/static/css/2.43786fff.chunk.css"
+  },
+  {
+    "revision": "35a04991a08cae2315ce",
+    "url": "/my-app/static/css/main.466c8c36.chunk.css"
+  },
+  {
+    "revision": "3dd337cd11f4ca0d6295",
+    "url": "/my-app/static/js/2.00ecb456.chunk.js"
+  },
+  {
+    "revision": "35a04991a08cae2315ce",
+    "url": "/my-app/static/js/main.4ae130ba.chunk.js"
+  },
+  {
+    "revision": "2db7b97f95a390132e73",
+    "url": "/my-app/static/js/runtime-main.5f72c099.js"
+  }
+]);
